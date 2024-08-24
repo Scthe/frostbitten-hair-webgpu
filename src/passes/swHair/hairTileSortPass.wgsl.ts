f3a872faef99d3830fe847c19db1b4b4b85d3352
Scthe <marcin1113C@gmail.com>
2024-08-24 18:23:14 +0200

    Add workgoup sort, but it's useless. Need global sort

--- src/passes/swHair/hairTileSortPass.wgsl.ts
+++ src/passes/swHair/hairTileSortPass.wgsl.ts
@@ -5,7 +5,7 @@ import * as SHADER_SNIPPETS from '../_shaderSnippets/shaderSnippets.wgls.ts';
 import { SHADER_TILE_UTILS } from './shaderImpl/tileUtils.wgsl.ts';
 
 export const SHADER_PARAMS = {
-  workgroupSizeX: 64, // TODO
+  workgroupSizeX: 256,
   bindings: {
     renderUniforms: 0,
     segmentCountPerTile: 1,
@@ -28,23 +28,83 @@ ${RenderUniformsBuffer.SHADER_SNIPPET(b.renderUniforms)}
 ${BUFFER_SEGMENT_COUNT_PER_TILE(b.segmentCountPerTile, 'read')}
 ${BUFFER_TILE_LIST(b.tileList, 'read_write')}
 
+var<workgroup> _sortList: array<u32, ${c.workgroupSizeX}>;
+var<workgroup> _tileCount: atomic<u32>;
+var<workgroup> _tileCount2: u32; // cannot use workgroupUniformLoad() on atomic
 
 @compute
 @workgroup_size(${c.workgroupSizeX}, 1, 1)
 fn main(
   @builtin(global_invocation_id) global_id: vec3<u32>,
+  @builtin(local_invocation_index) local_invocation_index: u32, // threadId inside workgroup
 ) {
   let tileIdx = global_id.x;
   let viewportSize: vec2f = _uniforms.viewport.xy;
 
-  let tileCount2d = getTileCount(vec2u(viewportSize));
-  let tileCount = tileCount2d.x * tileCount2d.y;
-  if (tileIdx >= tileCount) { return; }
+  if (local_invocation_index == 0u) {
+    atomicStore(&_tileCount, 0u);
+  }
+  workgroupBarrier();
+
+  let screenTileCount_2d = getTileCount(vec2u(viewportSize));
+  let screenTileCount = screenTileCount_2d.x * screenTileCount_2d.y;
+  let isValid = tileIdx < screenTileCount;
 
   let segmentCount = _hairSegmentCountPerTile[tileIdx];
-  if (segmentCount > 0u) {
+  if (isValid && segmentCount > 0u) {
+    let idx = atomicAdd(&_tileCount, 1u);
+
+    // sort key maxes out at 255 segments per tile
+    _sortList[idx] = (
+      (min(segmentCount, 255u) << 24) |
+      tileIdx
+    );
+  }
+  workgroupBarrier();
+
+  if (local_invocation_index == 0u) {
+    _tileCount2 = atomicLoad(&_tileCount);
+  }
+  workgroupBarrier();
+
+  // sorting
+  // TBH pointless, since we write in rng order when using _addTileToTaskQueue()
+  // TODO bucket-sort
+  let drawnTilesCount = workgroupUniformLoad(&_tileCount2);
+  sortLocalTiles(drawnTilesCount, local_invocation_index);
+  workgroupBarrier();
+    
+  if (isValid && local_invocation_index < drawnTilesCount) {
+    let tileIdx = _sortList[local_invocation_index] & 0x00ffffff;
     _addTileToTaskQueue(tileIdx);
   }
 }
 
+fn sortLocalTiles(arrLen: u32, threadId: u32) {
+  // empty tile. No segments, no sorting.
+  // Moved into for loop to prevent divergent warning
+  // if (arrLen == 0u){ return; }
+
+  for (
+    var i = 0u;
+    arrLen > 0u && i < arrLen - 1u;
+    i += 1u
+  ) {
+    let isOdd = (i & 1) > 0;
+    let offset = 2 * threadId + select(0u, 1u, isOdd);
+    let threadLimit = arrLen / 2u - select(0u, 1u, isOdd);
+
+    if ((offset + 1u) < arrLen && _sortList[offset] < _sortList[offset + 1u]) {
+      swapElements(offset, offset + 1);
+    }
+
+    workgroupBarrier();
+  }
+}
+
+fn swapElements(i: u32, j: u32) {
+  let temp = _sortList[i];
+  _sortList[i] = _sortList[j];
+  _sortList[j] = temp;
+}
 `;
